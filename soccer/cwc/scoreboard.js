4baa683f58f5a2b8006de030a1a16904adfb8fcf
Oluwadamilola Laraiyetan <laraiyeo@mcmaster.ca>
2025-07-04 17:01:51 -0400

    Update scoreboard.js

--- soccer/cwc/scoreboard.js
+++ soccer/cwc/scoreboard.js
@@ -35,7 +35,7 @@ function renderScorersBox(awayScorers, homeScorers) {
   const formatScorer = scorer => {
     const penaltyText = scorer.penaltyKick ? " (Pen.)" : "";
     const fullName = scorer.displayName;
-    const lastName = fullName. split(" "). slice (-1) • join(" ");
+    const lastName = fullName.split(" ").slice (-1).join(" ");
     const displayName = window.innerWidth <= 525 ? lastName : fullName;
     return `${displayName} ${scorer.clock}${penaltyText}`;
   };
