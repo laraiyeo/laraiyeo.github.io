79d0ce6188e05cd91f53206c3d9e3a3d0f89ee4c
Oluwadamilola Laraiyetan <laraiyeo@mcmaster.ca>
2025-08-25 19:44:37 -0400

    Update

---
+++ assets/analytics.js
@@ -0,0 +1,14 @@
+// analytics.js
+(function() {
+  var gtagScript = document.createElement("script");
+  gtagScript.async = true;
+  gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-37VMZQ1G0E";
+  document.head.appendChild(gtagScript);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+
+  gtag('js', new Date());
+  gtag('config', 'G-37VMZQ1G0E');
+})();
